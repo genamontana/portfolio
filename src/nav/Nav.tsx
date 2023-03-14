04e31a722f09f064a83953302907c04fb20ab555
genamontana <predator199@yandex.ru>
2023-03-14 20:07:01 +0300

    1 hour 55 min

--- src/nav/Nav.tsx
+++ src/nav/Nav.tsx
@@ -4,10 +4,10 @@ import s from './Nav.module.css'
 export const Nav = () => {
     return (
         <div className={s.nav}>
-            <a>Главная</a>
-            <a>Скиллы</a>
-            <a>Работы</a>
-            <a>Контакты</a>
+            <a href={'##'}>Главная</a>
+            <a href={'##'}>Скиллы</a>
+            <a href={'##'}>Работы</a>
+            <a href={'##'}>Контакты</a>
         </div>
     );
 };
