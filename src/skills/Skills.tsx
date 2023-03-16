16efa1ae05e81ad390703032f18c78cc1dd608b6
genamontana <predator199@yandex.ru>
2023-03-16 12:41:41 +0300

    1 hour 58 min

--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -9,9 +9,12 @@ export const Skills = () => {
             <div className={`${sContainer.container} ${s.skillsContainer}`}>
                 <h2 className={s.title}>Мои скиллы</h2>
                 <div className={s.skills}>
-                <Skill title={'JS'}/>
-                <Skill title={'HTML'}/>
-                <Skill title={'CSS'}/>
+                    <Skill title={'JS'}
+                           description={'Подробное описание навыка'}/>
+                    <Skill title={'HTML'}
+                           description={'Подробное описание навыка'}/>
+                    <Skill title={'CSS'}
+                           description={'Подробное описание навыка'}/>
                 </div>
             </div>
         </div>
--- src/skills/skill/Skill.tsx
+++ src/skills/skill/Skill.tsx
@@ -3,6 +3,7 @@ import s from './Skill.module.css'
 
 type SkillType = {
     title:string
+    description:string
 }
 
 export const Skill = (props:SkillType) => {
@@ -10,6 +11,7 @@ export const Skill = (props:SkillType) => {
         <div className={s.skill}>
             <div className={s.icon}></div>
             <h3>{props.title}</h3>
+            <div className={s.description}>{props.description}</div>
         </div>
     );
 };
