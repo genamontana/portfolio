7d9998b851dbd8275818a4e85d65c4cb878aa295
genamontana <predator199@yandex.ru>
2023-05-11 12:22:25 +0300

    in process second lecture 1h 25min

--- src/Contacts/Contacts.tsx
+++ src/Contacts/Contacts.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import s from './Contacts.module.css'
 import sContainer from '../common/styles/Container.module.css';
 import sButton from '../common/styles/Button.module.css'
+import {Title} from '../common/components/title/Title';
 
 export const Contacts = () => {
     return (
         <div className={s.contactsBlock}>
             <div className={`${sContainer.container} ${s.contactsContainer}`}>
-                <h2 className={s.title}>Контакты</h2>
+                <Title title={'Контакты'} span={'Давайте свяжемся'}/>
                 <form action={'##'} className={s.form}>
                     <input type="text" className={s.name}/>
                     <input type="text" className={s.surname}/>
--- src/freelance/Freelance.tsx
+++ src/freelance/Freelance.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import sContainer from '../common/styles/Container.module.css';
 import s from './Freelance.module.css'
 import sButton from '../common/styles/Button.module.css'
+import {Title} from '../common/components/title/Title';
 
 export const Freelance = () => {
     return (
         <div className={s.freelanceBlock}>
             <div className={`${sContainer.container} ${s.freelanceContainer}`}>
-                <h2 className={s.title}>Рассматриваю варианты удаленной работы</h2>
+                <Title title={'Рассматриваю варианты удаленной работы'} span={'Фриланс'}/>
                 <a className={sButton.button} href={'##'}>Нанять меня</a>
             </div>
         </div>
