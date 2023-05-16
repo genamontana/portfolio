789b030e5812c21c9f3072cacbadc01008ccdf74
genamontana <predator199@yandex.ru>
2023-05-16 08:35:39 +0300

    in process second lecture ULBITV Burger

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import './App.css';
+import {HeaderTest} from './headerTest/HeaderTest';
 import {Header} from './header/Header';
 import {Main} from './main/Main';
 import {Skills} from './skills/Skills';
 import {MyProjects} from './myProjects/MyProjects';
 import {Freelance} from './freelance/Freelance';
-import {Contacts} from './Contacts/Contacts';
+import {Contacts} from './contacts/Contacts';
 import {Footer} from './footer/Footer';
 
 function App() {
     return (
         <div className="App">
+            <HeaderTest/>
             <Header/>
             <Main/>
             <Skills/>
